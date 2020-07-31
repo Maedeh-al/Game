2f17310ddfb7c90360097366d1134c5fc8a3fd4a
Maedeh-al <58287161+Maedeh-al@users.noreply.github.com>
2020-07-31 16:50:04 +0430

    Add files via upload

---
+++ Game1,3,5.py
@@ -0,0 +1,22 @@
+m=int(input())
+
+while m>=0 and m<=9:
+    if m==1 or m==4 or m==6 or m==9:
+        print("Yes")
+    else:
+        print("No")
+
+    if m==3 or m==4 or m==8 or m==9:
+        print("Yes")
+    else:
+        print("No")
+
+    if m==5 or m==6 or m==8 or m==9:
+        print("Yes")
+    else:
+        print("No")
+
+    break
+
+
+
